c8e02724e7f8404b8a8bbe175226c7e5d4b02b3a
askat-makers <askatdovut1@gmail.com>
2022-03-02 17:28:13 +0600

    corrected1

--- src/components/shared/Container/index.tsx
+++ src/components/shared/Container/index.tsx
@@ -1,12 +1,1 @@
-import React from "react";
-import styled from "styled-components";
-
-export const Container: React.FC = ({ children }) => {
-  return <ContainerBlock>{children}</ContainerBlock>;
-};
-
-const ContainerBlock = styled.div`
-  max-width: 1200px;
-  width: 90%;
-  margin: 0 auto;
-`;
+export { Container } from "./Container";
